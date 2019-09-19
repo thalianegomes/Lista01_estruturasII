d740dc720d4c56b9fdb8aafb0fc47260fdb5d8c5
Samuel-OR <samueloliveira0014@gmail.com>
2019-09-19 01:35:42 -0300

    Concluido

--- Q7_AVL.c
+++ Q7_AVL.c
@@ -77,41 +77,45 @@ void exibirAVL(NO* raiz){
 	}
 }
 
-int alocar(NO** raiz, int qtdBlocos){
+int alocar(NO** raiz, NO** pai, int qtdBlocos){
 
 	//Resul 1 para alocação concluida
 	int resul = 0;
 	if( *raiz != NULL){
-
-		NO** pai = raiz;
-
-		resul = alocar(&(*raiz)->esq, qtdBlocos);
+		resul = alocar(&(*raiz)->esq,raiz, qtdBlocos);
 		if(resul == 0){
 			
 			if((*raiz)->status == 'L'){
 				int qtdLocal = (*raiz)->endFim - (*raiz)->endInicio +1;
 				
-	
 				//For folha
 				if((*raiz)->dir == NULL){
 					if(qtdLocal == qtdBlocos){
 						//Buscar os mais internos para unir;
-						int newInicio = (*pai)->endInicio - qtdBlocos;
-						(*pai)->endInicio = newInicio;
+						int newFim = (*pai)->endFim + qtdBlocos;
+						(*pai)->endFim = newFim;
 						free(*raiz);
+						*raiz = NULL;
 						resul = 1;
 					}else if(qtdLocal > qtdBlocos){
-						int newFim = (*raiz)->endFim - qtdLocal;
-						int newInicio = (*pai)->endInicio - qtdBlocos;
-						(*raiz)->endFim = newFim;
-						(*pai)->endInicio = newInicio;
+						if((*pai)->esq == *raiz){
+							int newFim = (*raiz)->endFim - qtdBlocos;
+							int newInicio = (*pai)->endInicio - qtdBlocos;
+							(*raiz)->endFim = newFim;
+							(*pai)->endInicio = newInicio;
+						}else{
+							int newInicio = (*raiz)->endInicio + qtdBlocos;
+							int newFim = (*pai)->endFim + qtdBlocos;
+							(*raiz)->endInicio = newInicio;
+							(*pai)->endFim = newFim;
+						}
 						resul = 1;
 					}
 				}else{
-
+			
 					//Buscando o mais a esquerda Ocupado.
-					NO** maisEsq = &((*raiz)->dir);
 					NO** paiMaisEsq = raiz;
+					NO** maisEsq = &((*raiz)->dir);
 					while((*maisEsq)->esq != NULL){
 						paiMaisEsq = maisEsq;
 						maisEsq = &((*maisEsq)->esq) ;
@@ -119,66 +123,185 @@ int alocar(NO** raiz, int qtdBlocos){
 					if((*paiMaisEsq)->status == 'O'){
 						maisEsq = paiMaisEsq;
 					}
-					free(paiMaisEsq);
-
-					//Buscando o mais a direita Ocupado.
-					NO** maisDir = &((*raiz)->esq);
-					NO** paiMaisDir = raiz;
-					while((*maisDir)->dir != NULL){
-						paiMaisDir = maisDir;
-						maisDir = &((*maisDir)->dir) ;
-					}
-					if((*paiMaisDir)->status == 'O'){
-						maisDir = paiMaisDir;
-					}
-					free(*paiMaisDir);
-
-
+					//free(*paiMaisEsq);		
+					
 					if(qtdLocal == qtdBlocos){
+
+						//Buscando o mais a direita Ocupado.
+						NO** paiMaisDir = raiz;
+						NO** maisDir = &((*raiz)->esq);
+						while((*maisDir)->dir != NULL){
+							paiMaisDir = maisDir;
+							maisDir = &((*maisDir)->dir) ;
+						}
+						if((*paiMaisDir)->status == 'O'){
+							maisDir = paiMaisDir;
+						}
+						//free(*paiMaisDir);
 						
 						//Buscar os mais internos para unir;
 						int newInicio = (*maisDir)->endInicio;
 						int newFim = (*maisEsq)->endFim;
 						(*raiz)->endInicio = newInicio;
 						(*raiz)->endFim = newFim;
+						(*raiz)->status = 'O';
+
 						
 						free(*maisDir);
 						free(*maisEsq);
-
+						*maisDir = NULL;
+						*maisEsq = NULL;	
 						resul = 1;
 					}else if(qtdLocal > qtdBlocos){
-						int newFim = (*raiz)->endFim - qtdLocal;
+						int newFim = (*raiz)->endFim - qtdBlocos;
 						int newInicio = (*maisEsq)->endInicio - qtdBlocos;
 						(*raiz)->endFim = newFim;
-						(*pai)->endInicio = newInicio;
+						(*maisEsq)->endInicio = newInicio;
 						resul = 1;
+					
 					}else{
-						resul = alocar(&(*raiz)->dir, qtdBlocos);
-					}
+						resul = alocar(&(*raiz)->dir,raiz, qtdBlocos);
+					}	
 				}
-
-
+			}else{
+				resul = alocar(&(*raiz)->dir,raiz, qtdBlocos);
 			}
 		}
 	}
 	return resul;
 }
 
+
+int liberar(NO** raiz, NO** pai, int qtdBlocos){
+
+	//Resul 1 para alocação concluida
+	int resul = 0;
+	if( *raiz != NULL){
+		resul = liberar(&(*raiz)->esq,raiz, qtdBlocos);
+		if(resul == 0){
+			
+			if((*raiz)->status == 'O'){
+				int qtdLocal = (*raiz)->endFim - (*raiz)->endInicio +1;
+				
+				//For folha
+				if((*raiz)->dir == NULL){
+					if(qtdLocal == qtdBlocos){
+						//Buscar os mais internos para unir;
+						int newFim = (*pai)->endFim + qtdBlocos;
+						(*pai)->endFim = newFim;
+						free(*raiz);
+						*raiz = NULL;
+						resul = 1;
+					}else if(qtdLocal > qtdBlocos){
+						if((*pai)->esq == *raiz){
+							int newFim = (*raiz)->endFim - qtdBlocos;
+							int newInicio = (*pai)->endInicio - qtdBlocos;
+							(*raiz)->endFim = newFim;
+							(*pai)->endInicio = newInicio;
+						}else{
+							int newInicio = (*raiz)->endInicio + qtdBlocos;
+							int newFim = (*pai)->endFim + qtdBlocos;
+							(*raiz)->endInicio = newInicio;
+							(*pai)->endFim = newFim;
+						}
+						resul = 1;
+					}
+				}else{
+			
+					//Buscando o mais a esquerda Ocupado.
+					NO** paiMaisEsq = raiz;
+					NO** maisEsq = &((*raiz)->dir);
+					while((*maisEsq)->esq != NULL){
+						paiMaisEsq = maisEsq;
+						maisEsq = &((*maisEsq)->esq) ;
+					}
+					if((*paiMaisEsq)->status == 'L'){
+						maisEsq = paiMaisEsq;
+					}
+					//free(*paiMaisEsq);		
+					
+					if(qtdLocal == qtdBlocos){
+
+						//Buscando o mais a direita Ocupado.
+						NO** paiMaisDir = raiz;
+						NO** maisDir = &((*raiz)->esq);
+						while((*maisDir)->dir != NULL){
+							paiMaisDir = maisDir;
+							maisDir = &((*maisDir)->dir) ;
+						}
+						if((*paiMaisDir)->status == 'L'){
+							maisDir = paiMaisDir;
+						}
+						//free(*paiMaisDir);
+	
+						//Buscar os mais internos para unir;
+						int newInicio = (*maisDir)->endInicio;
+						int newFim = (*maisEsq)->endFim;
+						(*raiz)->endInicio = newInicio;
+						(*raiz)->endFim = newFim;
+						(*raiz)->status = 'L';
+
+						
+						free(*maisDir);
+						free(*maisEsq);
+						*maisDir = NULL;
+						*maisEsq = NULL;	
+						resul = 1;
+					}else if(qtdLocal > qtdBlocos){
+						int newFim = (*raiz)->endFim - qtdBlocos;
+						int newInicio = (*maisEsq)->endInicio - qtdBlocos;
+						(*raiz)->endFim = newFim;
+						(*maisEsq)->endInicio = newInicio;
+						resul = 1;
+					
+					}else{
+						resul = liberar(&(*raiz)->dir,raiz, qtdBlocos);
+					}	
+				}
+			}else{
+				resul = liberar(&(*raiz)->dir,raiz, qtdBlocos);
+			}
+		}
+	}
+	return resul;
+}
+
+
 int main(){
 
 	NO* groot = criarArv();
-	int  status=1;
-	inserirAVL(&groot, criarFolha(&status, 1,100,1,10));
-	inserirAVL(&groot, criarFolha(&status, 1,100,11,20));
-	inserirAVL(&groot, criarFolha(&status, 1,100,21,35));
-	inserirAVL(&groot, criarFolha(&status, 1,100,36,50));
-	inserirAVL(&groot, criarFolha(&status, 1,100,51,70));
-	inserirAVL(&groot, criarFolha(&status, 1,100,71,85));
-	inserirAVL(&groot, criarFolha(&status, 1,100,86,90));
-	inserirAVL(&groot, criarFolha(&status, 1,100,91,100));
+	int  status=0;	//1-Ocupado 0-Livre
+	inserirAVL(&groot, criarFolha(&status, 1,200,1,10));
+	inserirAVL(&groot, criarFolha(&status, 1,200,11,20));
+	inserirAVL(&groot, criarFolha(&status, 1,200,21,35));
+	inserirAVL(&groot, criarFolha(&status, 1,200,36,50));
+	inserirAVL(&groot, criarFolha(&status, 1,200,51,70));
+	inserirAVL(&groot, criarFolha(&status, 1,200,71,85));
+	inserirAVL(&groot, criarFolha(&status, 1,200,86,90));
+	inserirAVL(&groot, criarFolha(&status, 1,200,91,150));
+	//inserirAVL(&groot, criarFolha(&status, 1,200,151,200));
+	
 	exibirAVL(groot);
+	printf("------------\n");
+	
+	if( alocar(&groot,NULL, 5) == 0)
+		printf("Espaço insuficiente.\n");
+	else
+		printf("Alocação bem sucedida.\n");
+	
+	exibirAVL(groot);
+	printf("------------\n");
 
-	alocar(&groot, 10);
+	if( liberar(&groot,NULL, 60) == 0)
+		printf("Espaço insuficiente.\n");
+	else
+		printf("Liberação bem sucedida.\n");
+	
+	exibirAVL(groot);
+	printf("------------\n");
 
+
+
+		
     return 0;
 }
